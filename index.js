10b5d7611d2c0e420596b5fcd2f2c91fe673aacc
Arun <arunarofficial00@gmail.com>
2024-02-01 01:18:04 +0530

    commit

--- index.js
+++ index.js
@@ -8,18 +8,30 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:4000",
   })
 );
 
+// app.use(
+//   cors({
+//     origin: "*", 
+//   })
+// );
+
 app.listen(4000, () => console.log("App is running http://localhost:4000"));
 
-app.get("/health", (req, res) => res.send(200));
+// data='cool is bad to health'
+// const sentiment = getSentiment(data);
+
+// console.log(sentiment)
 
 app.post("/api/sentiment", (req, res) => {
+  console.log(req)
   const data = req.body.data;
 
   const sentiment = getSentiment(data);
 
   return res.send({ sentiment });
 });
+
+
