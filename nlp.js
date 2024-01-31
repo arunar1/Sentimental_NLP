5c011f4cbb5c6084fa3ec30bd7d53ca2fd066f68
Arun <arunarofficial00@gmail.com>
2024-02-01 01:59:34 +0530

    commit

--- nlp.js
+++ nlp.js
@@ -1,4 +1,3 @@
-
 const aposToLexForm = require("apos-to-lex-form");
 const { WordTokenizer, SentimentAnalyzer, PorterStemmer } = require("natural");
 
@@ -12,24 +11,27 @@ spellCorrector.loadDictionary();
 const analyzer = new SentimentAnalyzer("English", PorterStemmer, "afinn");
 
 function getSentiment(str) {
-  if (!str.trim()) {
-    return 0;
+  // Check if the input is missing or empty
+  if (!str || !str.trim()) {
+    return 0; // Assuming missing or empty input is neutral
   }
 
-  const lexed = aposToLexForm(str)
-    .toLowerCase()
-    .replace(/[^a-zA-Z\s]+/g, "");
+  const lexed = str.toLowerCase().replace(/[^a-zA-Z\s]+/g, "");
 
   const tokenized = tokenizer.tokenize(lexed);
+  console.log(tokenized)
 
   const fixedSpelling = tokenized.map((word) => spellCorrector.correct(word));
-
+  console.log(fixedSpelling)
+   
   const stopWordsRemoved = stopword.removeStopwords(fixedSpelling);
+  console.log(stopWordsRemoved)
 
   const analyzed = analyzer.getSentiment(stopWordsRemoved);
+  console.log(analyzed)
 
-  if (analyzed >= 1) return 1; // positive
-  if (analyzed === 0) return 0;
+  if (analyzed >= 0.6) return 1; // positive
+  if (analyzed > 0 ) return 0;
   return -1;
 }
 
