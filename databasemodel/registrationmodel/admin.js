e4267c5b2ed503cca792df3e219bc8e6bb87e01e
Arun <arunarofficial00@gmail.com>
2024-02-25 23:38:00 +0530

    schema is added

--- databasemodel/registrationmodel/admin.js
+++ databasemodel/registrationmodel/admin.js
@@ -2,13 +2,17 @@ const mongoose = require('mongoose');
 
 const adminSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  adminId:{type:String , required:true },
   age: { type: Number, required: true },
   gender: { type: String, enum: ['Male', 'Female', 'Other'], required: true },
   constituency: { type: String, required: true },
+  constituencyId:{type:String,required},
   mobileNumber: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  userType: { type: String, enum: ['user', 'admin'], required: true }
+  userType: { type: String, enum: ['user', 'admin'], required: true },
+  adharImage:{type:String , required: true},
+  profileImage:{type:String, required:true}
 });
 
 const Admin = mongoose.model('Admin', adminSchema);
