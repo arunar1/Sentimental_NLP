fc2d9fdde3a784b355418fc5a51ef24a1efe6065
Arun <arunarofficial00@gmail.com>
2024-03-04 01:12:47 +0530

    update :error response handling

--- routers/verificationRouter.js
+++ routers/verificationRouter.js
@@ -3,29 +3,35 @@ const nodemailer = require('nodemailer');
 const admin = require('../databasemodel/registrationmodel/admin');
 const user = require('../databasemodel/registrationmodel/usermodel');
 
-const {generateRandomCode} =require('../RandomCodeGen/random')
+const { generateRandomCode } = require('../RandomCodeGen/random');
 
 const router = express.Router();
 
-
 router.post('/', async (req, res) => {
-
-    
-
-
     try {
         const data = req.body.info;
-        console.log(data)
+        console.log(data);
+
+        const gmailRegex = /^[a-zA-Z0-9._%+-]+@gmail.com$/;
+        if (!gmailRegex.test(data.email)) {
+            console.log("hello")
+            res.send({message:"Provide a valid email"});
+            return
+            // return res.status(400).send({ status: 400, message: "Invalid Gmail address" });
+        }
 
         const searchAadhar = { "aadharNo": data.aadharNo };
         const searchPhone = { "mobileNumber": data.mobileNumber };
+        const searchGmail={"email":data.email};
 
         const userRecord = await user.findOne(searchAadhar);
         const adminRecord = await admin.findOne(searchAadhar);
         const userRecord1 = await user.findOne(searchPhone);
         const adminRecord1 = await admin.findOne(searchPhone);
+        const userRecord_2 = await user.findOne(searchGmail);
+        const adminRecord_2 = await admin.findOne(searchGmail);
 
-        if (userRecord || adminRecord || userRecord1 || adminRecord1) {
+        if (userRecord || adminRecord || userRecord1 || adminRecord1 || userRecord_2 || adminRecord_2) {
             return res.status(200).send({ status: 200, message: "Account Already exists" });
         }
 
@@ -59,6 +65,7 @@ router.post('/', async (req, res) => {
                 return res.status(200).send({ status: "ok", message: "Email is sent" });
             }
         });
+        res.send({message:"Email send"})
     } catch (error) {
         console.error('Error in /verification endpoint:', error);
         return res.status(500).send({ status: "error", message: "Internal Server Error" });
